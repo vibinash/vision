095c7664f07c25834cea555444dbb249ee85f857
vibinash <vibinash.thomas@gmail.com>
2016-08-10 23:30:42 -0700

    Added comments and explanations for filtering and blurring

--- image_processing/smoothing/image_blurring_averaging.py
+++ image_processing/smoothing/image_blurring_averaging.py
@@ -4,7 +4,16 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('../../images/owl.jpg')
 
+# Blurring is achieved by convolving the image with a low pass filter kernel.
+# Useful for removing noise like high frequency content (like noise, edges) resulting
+# in edges being blurred
+
+# Averaging is done by convolving the image with a normalized box filter.
+# Takes the average of all the pixels under the kernel area and replaces the centeral
+# element with this average.
+
 # use a kernel of 5x5
+# alternativly use cv2.boxFilter()
 blur = cv2.blur(img, (5,5))
 
 plt.subplot(121), plt.imshow(img), plt.title('Original')
--- image_processing/smoothing/image_filtering.py
+++ image_processing/smoothing/image_filtering.py
@@ -5,6 +5,15 @@ from matplotlib import pyplot as plt
 img = cv2.imread('../../images/owl.jpg')
 
 kernel = np.ones((5,5), np.float32)/25
+
+# Images can be filtered with LPF, HPF etc
+# LPF removes noise and blurs the image
+# HPF helps finds edges in an image
+
+# Filtering with kernel results in: for each pixel, a 5x5 window is centered on
+# this pixel, all pixels falling within this window are summed up and the result
+# is then divided by 25. Same as computing the average of the pixel values inside
+# that window. This is performed for all pixels in the image
 dst = cv2.filter2D(img, -1, kernel)
 
 plt.subplot(121), plt.imshow(img), plt.title('Original')
