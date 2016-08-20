03bb5cd9d705fdab7d3d20f510cc50b06578eee4
vibinash <vibinash.thomas@gmail.com>
2016-08-19 17:18:08 -0700

    add few images

--- image_processing/morphological_transformation/structuring_kernel.py
+++ image_processing/morphological_transformation/structuring_kernel.py
@@ -2,9 +2,30 @@ import cv2
 
 # Rectangular Kernel
 cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+'''
+array([[1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1]], dtype=uint8)
+'''
 
 # Elliptical Kernel
 cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+'''
+array([[0, 0, 1, 0, 0],
+       [1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1],
+       [0, 0, 1, 0, 0]], dtype=uint8)
+'''
 
 # Cross-shaped Kernel
 cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
+'''
+array([[0, 0, 1, 0, 0],
+       [0, 0, 1, 0, 0],
+       [1, 1, 1, 1, 1],
+       [0, 0, 1, 0, 0],
+       [0, 0, 1, 0, 0]], dtype=uint8)
+'''
