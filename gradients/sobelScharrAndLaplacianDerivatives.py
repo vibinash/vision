79444af7a7d0a6b7041872b9d9db45fb345559db
Vibinash <vibinash.thomas@gmail.com>
2016-08-26 00:01:40 -0700

    updated sobel/scharr

--- gradients/sobelScharrAndLaplacianDerivatives.py
+++ gradients/sobelScharrAndLaplacianDerivatives.py
@@ -4,9 +4,15 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('../images/receipt.jpg',0)
 
+# Gradient or High Pass Filter
+# Sobel - joint Gaussian smoothing plus differentiation opertations (more resistant to noise)
+# direction of the derivatives (vertical - yorder, horizontal - xorder)
+# ksize - size of the kernel
+# ksize = -1, a 3x3 Scharr filter is used
+
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
-sobelx = cv2.Sobel(img, cv2.CV_64F,1,0,ksize=5)
-sobely = cv2.Sobel(img, cv2.CV_64F,0,1,ksize=5)
+sobelx = cv2.Sobel(img, cv2.CV_64F,1,0,ksize=3)
+sobely = cv2.Sobel(img, cv2.CV_64F,0,1,ksize=3)
 
 plt.subplot(2,2,1), plt.imshow(img, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
