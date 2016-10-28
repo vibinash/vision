c1779deedcccf1b1a211ef2d150b4730874d43ee
Vibinash <vibinash.thomas@gmail.com>
2016-10-28 13:41:01 -0700

    Updated the sobel and scharr derivatives

--- gradients/sobelScharrAndLaplacianDerivatives.py
+++ gradients/sobelScharrAndLaplacianDerivatives.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('../images/receipt.jpg',0)
+img = cv2.imread('../images/puzzle.jpg',0)
 
 # Gradient or High Pass Filter
 # Sobel - joint Gaussian smoothing plus differentiation opertations (more resistant to noise)
@@ -12,7 +12,9 @@ img = cv2.imread('../images/receipt.jpg',0)
 
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
 sobelx = cv2.Sobel(img, cv2.CV_64F,1,0,ksize=3)
+abs_sobelx = np.absolute(sobelx)
 sobely = cv2.Sobel(img, cv2.CV_64F,0,1,ksize=3)
+abs_sobely = np.absolute(sobely)
 
 plt.subplot(2,2,1), plt.imshow(img, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
@@ -20,10 +22,10 @@ plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(2,2,2), plt.imshow(laplacian, cmap = 'gray')
 plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2,2,3), plt.imshow(sobelx, cmap = 'gray')
+plt.subplot(2,2,3), plt.imshow(abs_sobelx, cmap = 'gray')
 plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2,2,4), plt.imshow(sobely, cmap = 'gray')
+plt.subplot(2,2,4), plt.imshow(abs_sobely, cmap = 'gray')
 plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
 plt.show()
